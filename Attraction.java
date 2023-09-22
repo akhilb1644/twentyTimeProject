3319d33447db1929245d734fafb5b4a0ecadedb4
Akhil B <48994987+akhilmanhattan@users.noreply.github.com>
2023-09-22 14:46:20 -0400

    Update Attraction.java

--- Attraction.java
+++ Attraction.java
@@ -5,7 +5,17 @@ class Attraction{
   - Index zero first dimension will have all names
   - Index one first dimension will have all coordinates from Google Maps
   */
-  private attractionName;
-  private attractionCoords;
-  private attraction
+  private String attractionName;
+  private double[] attractionCoords = new double[2];
+  private byte coordsRealLength = 0;
+  private String[] attractionInterests = new String[5];
+  private byte interestsRealLength = 0;
+  private String[] attractionSubInterests = new String[10];
+  private byte subInterestsRealLength = 0;
+  private String[] attractionStates = new String[4];
+  private byte attractionStatesRealLength = 0;
+
+  public Attraction(String name, double[] coords, String interest, String subInterest, String state) {
+    
+  }
 }
