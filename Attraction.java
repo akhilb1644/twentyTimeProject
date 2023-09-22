2aacef745e840753d2c2000024c1d2bf7b62060e
Akhil B <48994987+akhilmanhattan@users.noreply.github.com>
2023-09-22 14:13:04 -0400

    Update Attraction.java

--- Attraction.java
+++ Attraction.java
@@ -1,3 +1,11 @@
 class Attraction{
-  // In txt, the order for dimensions are interest, subinterest, state
+  /* 
+  - In txt, the order for dimensions are type, interest, subinterest, state
+  - Number of parks included will be a one percent sample.
+  - Index zero first dimension will have all names
+  - Index one first dimension will have all coordinates from Google Maps
+  */
+  private attractionName;
+  private attractionCoords;
+  private attraction
 }
